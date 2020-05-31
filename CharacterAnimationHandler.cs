705787e5459a59596eb873e80807ebdc8aa0d94c
UmairKhanJadoon <57166658+UmairKhanJadoon@users.noreply.github.com>
2020-05-31 18:42:26 +0500

    Add files via upload
    Code for The Project

---
+++ CharacterAnimationHandler.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWS;
+
+public class CharacterAnimationHandler : MonoBehaviour
+{
+    public Animator characterAnimator;
+    public GameObject childCamera;
+
+
+
+    private void Start()
+    {
+        characterAnimator = GetComponent<Animator>();
+        
+
+
+        if (PlayerPrefs.GetInt("Mina") == 1)
+        {
+            characterAnimator.SetBool("isIdle", true);
+            StartCoroutine(characterAnimationDelay());
+        }
+
+        if (PlayerPrefs.GetInt("Mina2") == 1)
+        {
+            characterAnimator.SetBool("isIdle", true);
+            StartCoroutine(characterAnimationDelay2());
+        }
+
+        if (PlayerPrefs.GetInt("JamaratUlUqbah") == 1)
+        {
+            //characterAnimator.SetBool("isWalkingPlace", true);
+            characterAnimator.SetBool("isIdle", true);
+            childCamera.SetActive(true);
+            StartCoroutine(JamraatDelay());
+        }
+    }
+
+
+
+    // Mina1 character starting Animation delays...............
+
+    IEnumerator characterAnimationDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        characterAnimator.SetBool("isIdle", false);
+        characterAnimator.SetBool("isWalking", true);
+        GamePlay.GP.source.PlayOneShot(GamePlay.GP.MyClips[0], 2f);
+        yield return new WaitForSeconds(15f);
+        characterAnimator.SetBool("isWalking", false);
+        characterAnimator.SetBool("isIdle", true);
+
+    }
+
+    // Mina2 character starting Animation delays...............
+
+    IEnumerator characterAnimationDelay2()
+    {
+        yield return new WaitForSeconds(2f);
+        characterAnimator.SetBool("isIdle", false);
+        characterAnimator.SetBool("isWalking", true);
+        //GamePlay.GP.source.PlayOneShot(GamePlay.GP.MyClips[0], 2f);
+        yield return new WaitForSeconds(15f);
+        characterAnimator.SetBool("isWalking", false);
+        characterAnimator.SetBool("isIdle", true);
+
+    }
+
+    // Jamraat character starting Animation delays...............
+
+    IEnumerator JamraatDelay()
+    {
+       
+        this.gameObject.GetComponent<splineMove>().moveToPath = true;
+        this.gameObject.GetComponent<splineMove>().pathContainer = GameObject.FindObjectOfType<PathManager>();
+        characterAnimator.SetBool("isIdle", false);
+        characterAnimator.SetBool("isWalkingPlace", true);
+        yield return new WaitForSeconds(10f);
+        characterAnimator.SetBool("isWalkingPlace", false);
+        characterAnimator.SetBool("isThrow", true);
+        characterAnimator.SetBool("isIdle", false);
+        GamePlay.GP.source.PlayOneShot(GamePlay.GP.MyClips[0], 2f);
+        StartCoroutine(StoneThrow());
+
+
+
+    }
+    int stoneCount = 1;
+    IEnumerator StoneThrow()
+    {
+       
+        yield return new WaitForSeconds(2.23f);
+        characterAnimator.SetBool("isThrow", false);
+        characterAnimator.SetBool("isIdle", true);
+        yield return new WaitForSeconds(2f);
+        characterAnimator.SetBool("isIdle", false);
+        characterAnimator.SetBool("isThrow", true);
+
+        stoneCount += 1;
+        
+       
+        if (stoneCount <= 7)
+        {
+            GamePlay.GP.source.PlayOneShot(GamePlay.GP.MyClips[0], 2f);
+            StartCoroutine(StoneThrow());
+
+            Debug.Log("Anim continued");
+        }
+        else
+        {
+            StopCoroutine(StoneThrow());
+            characterAnimator.SetBool("isIdle", true);
+            characterAnimator.SetBool("isThrow", false);
+            Debug.Log("Anim finished");
+        }
+        
+
+    }
+
+
+
+
+
+        public void IhramAction()
+    {
+        StopCoroutine(IhraamAnimationDelay());
+        StartCoroutine(IhraamAnimationDelay());
+
+    }
+
+
+    // Ihraam Guide Animations.....
+    IEnumerator IhraamAnimationDelay()
+    {
+        GamePlay.GP.ihraamInstructions.SetActive(true);
+        characterAnimator.SetBool("isIhram", true);
+        characterAnimator.SetBool("isIdle", false);
+        yield return new WaitForSeconds(2.26f);
+        characterAnimator.SetBool("isIhram", false);
+        characterAnimator.SetBool("isIdle", true);
+        GamePlay.GP.ihraamInstructions.SetActive(false);
+        StopCoroutine(IhraamAnimationDelay());
+    }
+
+}
---
+++ mainMenu.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public GameObject[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PlayerPrefs.DeleteAll();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+
+
+    public void EightDay()
+    {
+        buttons[0].SetActive(true);
+        buttons[1].SetActive(true);
+        buttons[2].SetActive(false);
+        buttons[3].SetActive(false);
+        buttons[4].SetActive(false);
+        buttons[5].SetActive(false);
+        buttons[6].SetActive(false);
+        buttons[7].SetActive(false);
+        buttons[8].SetActive(false);
+        buttons[9].SetActive(false);
+        buttons[10].SetActive(false);
+        buttons[11].SetActive(false);
+
+    }
+    public void NinthDay()
+    {
+        buttons[0].SetActive(false);
+        buttons[1].SetActive(false);
+        buttons[2].SetActive(true);
+        buttons[3].SetActive(true);
+        buttons[4].SetActive(true);
+        buttons[5].SetActive(false);
+        buttons[6].SetActive(false);
+        buttons[7].SetActive(false);
+        buttons[8].SetActive(false);
+        buttons[9].SetActive(false);
+        buttons[10].SetActive(false);
+        buttons[11].SetActive(false);
+    }
+
+    public void TenthDay()
+    {
+        buttons[0].SetActive(false);
+        buttons[1].SetActive(false);
+        buttons[2].SetActive(false);
+        buttons[3].SetActive(false);
+        buttons[4].SetActive(false);
+        buttons[5].SetActive(true);
+        buttons[6].SetActive(true);
+        buttons[7].SetActive(true);
+        buttons[8].SetActive(true);
+        buttons[9].SetActive(false);
+        buttons[10].SetActive(false);
+        buttons[11].SetActive(false);
+    }
+
+    public void EleventhDay()
+    {
+        buttons[0].SetActive(false);
+        buttons[1].SetActive(false);
+        buttons[2].SetActive(false);
+        buttons[3].SetActive(false);
+        buttons[4].SetActive(false);
+        buttons[5].SetActive(false);
+        buttons[6].SetActive(false);
+        buttons[7].SetActive(false);
+        buttons[8].SetActive(false);
+        buttons[9].SetActive(true);
+        buttons[10].SetActive(false);
+        buttons[11].SetActive(false);
+        buttons[12].SetActive(true);
+
+    }
+
+    public void TwelvethDay()
+    {
+        buttons[0].SetActive(false);
+        buttons[1].SetActive(false);
+        buttons[2].SetActive(false);
+        buttons[3].SetActive(false);
+        buttons[4].SetActive(false);
+        buttons[5].SetActive(false);
+        buttons[6].SetActive(false);
+        buttons[7].SetActive(false);
+        buttons[8].SetActive(false);
+        buttons[9].SetActive(false);
+        buttons[10].SetActive(true);
+        buttons[11].SetActive(true);
+    }
+
+    //scene changingn to 8thday of HAJJ
+    
+    public void MovetoMakkah()
+    {
+
+        SceneManager.LoadScene("8thDay");
+        PlayerPrefs.SetInt("Makkah", 1);
+    }
+    public void MovetoMina()
+    {
+
+        SceneManager.LoadScene("8thDay");
+        PlayerPrefs.SetInt("Mina",1);
+
+    }
+    //Scene changing to 9th day of HAjj
+    public void MovetoArafatAndMasjidNimra()
+    {
+
+        SceneManager.LoadScene("9thDay");
+        PlayerPrefs.SetInt("ArafatANDMasjidNimra", 1);
+    }
+
+  
+
+    public void MovetoMuzdalifa()
+    {
+        SceneManager.LoadScene("9thDay");
+        PlayerPrefs.SetInt("Muzdalifa",1);
+    }
+
+
+    //Scnene Changing to 10th day of Hajj
+    public void MovetoMina2()
+    {
+        SceneManager.LoadScene("10thDay");
+        PlayerPrefs.SetInt("Mina2",1);
+
+    }
+
+    public void MovetoJamaratUlUqbah()
+    {
+        SceneManager.LoadScene("10thDay");
+        PlayerPrefs.SetInt("JamaratUlUqbah",1);
+
+    }
+
+    public void MovetoQurbani()
+    {
+        SceneManager.LoadScene("10thDay");
+        PlayerPrefs.SetInt("Qurbani", 1);
+        
+
+    }
+
+    public void MovetoHalq()
+    {
+        SceneManager.LoadScene("10thDay");
+        PlayerPrefs.SetInt("Halq", 1);
+    }
+
+
+    //Scene changing to 11th day of hajj
+    public void MovetoJamarat()
+    {
+        SceneManager.LoadScene("11thDay");
+        PlayerPrefs.SetInt("Jamarat1", 1);
+    }
+    public void MovetoTawaafeZiyaraah()
+    {
+        SceneManager.LoadScene("11thDay");
+        PlayerPrefs.SetInt("Tawaaf", 1);
+    }
+
+    // Scene changes to 12th Day of Hajj
+    public void MovetoJamarat2()
+    {
+        SceneManager.LoadScene("12thDay");
+        PlayerPrefs.SetInt("Jamarat2", 1);
+    }
+
+    public void MovetoTawafWida()
+    {
+        SceneManager.LoadScene("12thDay");
+        PlayerPrefs.SetInt("TawafWida", 1);
+        
+    }
+
+    public void MenuFalse()
+    {
+        for (int i = 0; i <= buttons.Length - 1; i++)
+        {
+            buttons[i].SetActive(false);
+        }
+    }
+}
+
+
